d6ae08a8190ed13306d79f90b732bbcca73acc65
Nictauro <ntaurozzi@frba.utn.edu.ar>
2024-11-16 17:55:32 -0300

    Data augmentation functions added

--- packages/python/data/augmentation.py
+++ packages/python/data/augmentation.py
@@ -1,8 +1,12 @@
-from torchvision import transforms
-from PIL import Image
+import os
+import cv2
 import numpy as np
-from typing import Tuple, List
+import albumentations as A
 
+from PIL import Image
+from .utils import find_files
+from typing import Tuple, List
+from torchvision import transforms
 
 def cut_image(
     image: Image.Image, height: int, width: int, overlapX: int, overlapY: int
@@ -71,3 +75,37 @@ def change_image_basic(
     augmented_target_image = transform_target(target_image)
 
     return augmented_image, augmented_target_image
+
+
+def augment_folder(path, ext='.png'):
+    #Get a list of all images in folder
+    files = find_files(path, ext)
+    images_paths = files[list(files.keys())[1]]
+
+    # Define the augmentation pipeline
+    transform = A.Compose([
+        A.RandomRotate90(),
+        A.HorizontalFlip(),
+        A.VerticalFlip(),
+        A.RandomBrightnessContrast(p=0.4),
+        A.RandomGamma(p=1, gamma_limit=(60, 110)),
+        A.GaussNoise(p=0.2)
+    ])
+
+    for idx, image_path in enumerate(images_paths):
+        print(f"Image: {idx + 1}/{len(image_path)}", end='\r')
+        
+        # Load an image and apply the transformation
+        image_name = os.path.basename(image_path)
+        path = os.path.dirname(image_path)
+        base, ext = os.path.splitext(image_name)
+        
+        image = cv2.imread(image_path)
+        augmented_image = transform(image=image)['image']
+
+        new_name = f"{base}_augmented{ext}"
+        if os.path.exists(f"{path}/{new_name}"):
+            new_name = f"{base}_augmented_1{ext}"
+
+        # Save the augmented image
+        cv2.imwrite(f"{path}/{new_name}", augmented_image)
