dd1032009e7057d7df1f4c6797c8dbf484c20838
Nicolas Taurozzi <ntaurozzi@frba.utn.edu.ar>
2025-10-26 16:54:00 -0300

    initial commit

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Paths
 MODELS_PATH = os.path.join(BASE_DIR, 'models')
 DATASETS_PATH = os.path.join(BASE_DIR, 'datasets')
+IMG_SHAPE = (200, 200)
+SEED = 42
 # TEMP_PATH = os.path.join(BASE_DIR, 'temp')
 # RESULTS_PATH = os.path.join(BASE_DIR, 'results')
 # IMAGES_PATH = os.path.join(DATASETS_PATH, 'images')
